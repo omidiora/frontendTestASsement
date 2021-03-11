9cd273b802bc93f4812d738d45d3b57fab6788a7
omidiora <omidioraemmanuel@gmail.com>
2021-03-11 15:25:31 +0100

    Heroku

--- src/component/Personal.js
+++ src/component/Personal.js
@@ -55,7 +55,7 @@ function Personal() {
             showConfirmButton: false,
             timer: 1000000
           });
-          window.location = "/";
+        
       
         })
         .catch(err => console.log(err))
